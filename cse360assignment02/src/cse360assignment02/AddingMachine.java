cbcae617c2632aa494ff46bc5233b487f94a4e5c
Jemiah <Jemiah@Titan>
2020-10-06 14:20:31 -0700

    Initial commit of project.

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,47 @@
+package cse360assignment02;
+
+/**
+ * @author Jemiah
+ *
+ */
+public class AddingMachine {
+  private int total;
+
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  /**
+   * 
+   * This method will return the current total held in total variable.
+   * @return
+   */
+  public int getTotal () {
+    return 0;
+  }
+  
+  /**
+   * This method will add the passed in value to the total variable.
+ * @param value
+ */
+public void add (int value) {
+  }
+
+  /**
+   * This method will subtract the passed in value from the total variable.
+ * @param value
+ */
+public void subtract (int value) {
+  }
+/**
+ * 
+ */
+  public String toString () {
+    return "";
+  }
+
+/**
+ * This method clears the all value from the AddingMachine.
+ */
+public void clear() {
+  }
+}
